e081af803674b8fbd68b1b178980fb4654e0bd9f
omrobbie <putu.roby@yahoo.com>
2019-08-16 16:13:42 +0800

    create empty view controller for layout

---
+++ HotCoffee/Controllers/AddOrderViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AddOrderViewController.swift
+//  HotCoffee
+//
+//  Created by omrobbie on 16/08/19.
+//  Copyright © 2019 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class AddOrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ HotCoffee/Controllers/OrderTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  OrderTableViewController.swift
+//  HotCoffee
+//
+//  Created by omrobbie on 16/08/19.
+//  Copyright © 2019 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class OrderTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
