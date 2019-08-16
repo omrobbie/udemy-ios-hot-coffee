956ed8340198a99da2321e757508522ed3a63c05
omrobbie <putu.roby@yahoo.com>
2019-08-16 20:17:35 +0800

    create order model

---
+++ HotCoffee/Models/Order.swift
@@ -0,0 +1,29 @@
+//
+//  Order.swift
+//  HotCoffee
+//
+//  Created by omrobbie on 16/08/19.
+//  Copyright © 2019 omrobbie. All rights reserved.
+//
+
+import Foundation
+
+enum CoffeeType: String, Codable {
+    case cappuccino
+    case latte
+    case espressino
+    case cortado
+}
+
+enum CoffeeSize: String, Codable {
+    case small
+    case medium
+    case large
+}
+
+struct Order: Codable {
+    let name: String
+    let email: String
+    let type: CoffeeType
+    let size: CoffeeSize
+}
