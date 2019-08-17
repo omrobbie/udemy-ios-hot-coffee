a1422f6e12c9a09e02854480de8f346564fab302
omrobbie <putu.roby@yahoo.com>
2019-08-17 10:52:13 +0800

    displaying orders to table

--- HotCoffee/Controllers/OrderTableViewController.swift
+++ HotCoffee/Controllers/OrderTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class OrderTableViewController: UITableViewController {
 
+    var orderListViewModel = OrderListViewModel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         populateOrders()
@@ -22,13 +24,33 @@ class OrderTableViewController: UITableViewController {
 
         let resource = Resource<[Order]>(url: coffeeOrdersURL)
 
-        WebService().load(resource: resource) { result in
+        WebService().load(resource: resource) { [weak self] result in
             switch result {
             case .success(let orders):
                 print(orders)
+                self?.orderListViewModel.orderViewModel = orders.map(OrderViewModel.init)
+                self?.tableView.reloadData()
             case .failure(let error):
                 print(error)
             }
         }
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return orderListViewModel.orderViewModel.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OrderTableViewCell", for: indexPath)
+        let item = orderListViewModel.orderViewModel(at: indexPath.row)
+
+        cell.textLabel?.text = item.type
+        cell.detailTextLabel?.text = item.size
+
+        return cell
     }
 }
