fc16bde930885f0e0e0d429e2220196cdddbdc13
omrobbie <putu.roby@yahoo.com>
2019-08-17 09:48:13 +0800

    testing web service

--- HotCoffee/Controllers/OrderTableViewController.swift
+++ HotCoffee/Controllers/OrderTableViewController.swift
@@ -12,5 +12,23 @@ class OrderTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        populateOrders()
+    }
+
+    private func populateOrders() {
+        guard let coffeeOrdersURL = URL(string: "https://guarded-retreat-82533.herokuapp.com/orders") else {
+            fatalError("URL was incorrect!")
+        }
+
+        let resource = Resource<[Order]>(url: coffeeOrdersURL)
+
+        WebService().load(resource: resource) { result in
+            switch result {
+            case .success(let orders):
+                print(orders)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
