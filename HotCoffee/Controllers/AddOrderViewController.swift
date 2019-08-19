7a834054e4c57332081ea77ae561ed3a542171ec
omrobbie <putu.roby@yahoo.com>
2019-08-19 11:14:23 +0800

    connect add order view model to the layout

--- HotCoffee/Controllers/AddOrderViewController.swift
+++ HotCoffee/Controllers/AddOrderViewController.swift
@@ -10,7 +10,27 @@ import UIKit
 
 class AddOrderViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
+    private var viewModel = AddCoffeeOrderViewModel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+extension AddOrderViewController: UITableViewDelegate, UITableViewDataSource {
+
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.types.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CoffeeTypeTableViewCell", for: indexPath)
+
+        cell.textLabel?.text = viewModel.types[indexPath.row]
+
+        return cell
     }
 }
